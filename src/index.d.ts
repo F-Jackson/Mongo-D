606f62a2938f81c1f43c5716ef6c801850185b0a
FJackson <jacksonfinal@gmail.com>
2024-12-16 19:06:29 -0300

    making types

---
+++ src/index.d.ts
@@ -0,0 +1,25 @@
+declare module '@fjackson/mongo-d' {
+    import mongoose from 'mongoose';
+
+    export class InitMongoModels {
+        models: Record<string, mongoose.Model<any>>;
+        relations: Record<string, any>;
+        oldRelations: Record<string, any>;
+        Schema: mongoose.Schema;
+
+        constructor();
+
+        addRelations(fks: [string, any][], modelName: string): void;
+        removeRelations(name: string): void;
+        saveRelations(): void;
+        resetRelations(): void;
+        NewSchema(obj: any, options?: mongoose.SchemaOptions): mongoose.Schema;
+        MongoModel(
+            name: string,
+            schema: mongoose.Schema,
+            collection?: string,
+            options?: mongoose.ModelOptions,
+            __mocks?: any
+        ): Promise<mongoose.Model<any>>;
+    }
+}

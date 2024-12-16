3ba6187f05ae26d35f13b5f29df2192f4239dab8
FJackson <jacksonfinal@gmail.com>
2024-12-16 18:19:40 -0300

    testing dist creation

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/index.js', // Entry point of your library
@@ -12,7 +13,9 @@ export default {
   plugins: [
     nodeResolve(),
     commonjs(),
+    json(), // Adicionar o plugin JSON aqui
     babel({
+      babelHelpers: 'bundled', // Configurar explicitamente o babelHelpers
       presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
     }),
   ],

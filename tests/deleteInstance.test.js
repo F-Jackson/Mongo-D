533220fe6d5d73443bcc49a959c92ef2758c957b
FJackson <jacksonfinal@gmail.com>
2024-12-20 16:45:17 -0300

    fsgd

--- tests/deleteInstance.test.js
+++ tests/deleteInstance.test.js
@@ -187,7 +187,12 @@ describe("Mongo model Delete", () => {
             }
         ]);
         expect(tests).toHaveLength(2);
-        await RelatedModel.Delete({ _id: related._id });
+
+        const [ deletedCount, relatedCount, records ] = await RelatedModel.Delete({ _id: related._id });
+
+        expect(deletedCount).toEqual(1);
+        expect(relatedCount).toEqual(2);
+        expect(records["TestModel"].updated).toHaveLength(2);
 
         const testes = await TestModel.find({});
         expect(testes).toHaveLength(2);
@@ -233,7 +238,12 @@ describe("Mongo model Delete", () => {
             }
         ]);
         expect(tests).toHaveLength(2);
-        await RelatedModel.Delete({ _id: related._id });
+
+        const [ deletedCount, relatedCount, records ] = await RelatedModel.Delete({ _id: related._id });
+
+        expect(deletedCount).toEqual(1);
+        expect(relatedCount).toEqual(2);
+        expect(records["TestModel"].updated).toHaveLength(2);
 
         const testes = await TestModel.find({});
         expect(testes).toHaveLength(2);
@@ -280,7 +290,11 @@ describe("Mongo model Delete", () => {
         expect(tests[0].related).toHaveLength(3);
         expect(tests[1].related).toHaveLength(3);
 
-        await RelatedModel.Delete({ _id: related._id });
+        const [ deletedCount, relatedCount, records ] = await RelatedModel.Delete({ _id: related._id });
+
+        expect(deletedCount).toEqual(1);
+        expect(relatedCount).toEqual(2);
+        expect(records["TestModel"].updated).toHaveLength(2);
 
         const testes = await TestModel.find({});
         expect(testes).toHaveLength(2);
@@ -321,7 +335,12 @@ describe("Mongo model Delete", () => {
             }
         ]);
         expect(tests).toHaveLength(2);
-        await RelatedModel.Delete({ _id: related._id });
+
+        const [ deletedCount, relatedCount, records ] = await RelatedModel.Delete({ _id: related._id });
+
+        expect(deletedCount).toEqual(1);
+        expect(relatedCount).toEqual(2);
+        expect(records["TestModel"].excluded).toHaveLength(2);
 
         const testes = await TestModel.find({});
         expect(testes).toHaveLength(0);
@@ -366,7 +385,12 @@ describe("Mongo model Delete", () => {
             }
         ]);
         expect(tests).toHaveLength(2);
-        await RelatedModel.Delete({ _id: related._id });
+
+        const [ deletedCount, relatedCount, records ] = await RelatedModel.Delete({ _id: related._id });
+
+        expect(deletedCount).toEqual(1);
+        expect(relatedCount).toEqual(2);
+        expect(records["TestModel"].excluded).toHaveLength(2);
 
         const testes = await TestModel.find({});
         expect(testes).toHaveLength(0);
@@ -463,7 +487,12 @@ describe("Mongo model Delete", () => {
             }
         ]);
         expect(tests).toHaveLength(2);
-        await RelatedModel.Delete({ _id: related._id }, "keep");
+
+        const [ deletedCount, relatedCount, records ] = await RelatedModel.Delete({ _id: related._id }, "keep");
+
+        expect(deletedCount).toEqual(1);
+        expect(relatedCount).toEqual(2);
+        expect(records["TestModel"].updated).toHaveLength(2);
 
         const testes = await TestModel.find({});
         expect(testes).toHaveLength(2);
@@ -510,7 +539,12 @@ describe("Mongo model Delete", () => {
             }
         ]);
         expect(tests).toHaveLength(2);
-        await RelatedModel.Delete({ _id: related._id }, "keep");
+
+        const [ deletedCount, relatedCount, records ] = await RelatedModel.Delete({ _id: related._id }, "keep");
+
+        expect(deletedCount).toEqual(1);
+        expect(relatedCount).toEqual(2);
+        expect(records["TestModel"].updated).toHaveLength(2);
 
         const testes = await TestModel.find({});
         expect(testes).toHaveLength(2);
@@ -616,7 +650,11 @@ describe("Mongo model Delete", () => {
         expect(tests[0].nested.related).toHaveLength(3);
         expect(tests[1].nested.related).toHaveLength(3);
 
-        await RelatedModel.Delete({ _id: related._id });
+        const [ deletedCount, relatedCount, records ] = await RelatedModel.Delete({ _id: related._id });
+
+        expect(deletedCount).toEqual(1);
+        expect(relatedCount).toEqual(2);
+        expect(records["TestModel"].updated).toHaveLength(2);
 
         const testes = await TestModel.find({});
         expect(testes).toHaveLength(2);

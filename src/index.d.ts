cf2c90749f9477ff230d052ca9b8ee84e1163b60
FJackson <jacksonfinal@gmail.com>
2024-12-20 18:12:08 -0300

    dghf

--- src/index.d.ts
+++ src/index.d.ts
@@ -12,6 +12,19 @@ declare module '@fjackson/mongo-d' {
         options?: mongoose.ModelOptions
     ) => mongoose.Model<any, unknown, unknown, unknown, any, any>;
 
+    export interface ExtendedModel<T = any> extends mongoose.Model<T> {
+        Create: (doc: T, checkExistence?: boolean, callback?: mongoose.Callback) => Promise<T>;
+        Delete: (conditions: any, options?: any) => Promise<any>;
+    }
+
+    export const Model: (
+        mongoose: typeof mongoose,
+        name: string,
+        schema: mongoose.Schema,
+        collection?: string,
+        options?: mongoose.ModelOptions
+    ) => ExtendedModel;
+
     export const InitModels: (
         client: typeof mongoose
     ) => Promise<void>;

e851c90d3addb9fe8b81d8ba99398bdee22cba81
FJackson <jacksonfinal@gmail.com>
2024-12-26 10:59:04 -0300

    thdt

--- tests/createAggregate.test.js
+++ tests/createAggregate.test.js
@@ -1,8 +1,9 @@
 import { describe, it, beforeEach, expect } from "vitest";
-import mongoose, { Collection } from "mongoose";
+import mongoose from "mongoose";
 import { cleanDb, disconnectDb } from "./utils.js";
 import { InitModels, Model, Schema } from "../src/index.js";
 import { GenerateFoward } from "../src/aggregateGenerator/foward.js";
+const util = require('util');
 
 
 describe("Aggregate Foward", () => {
@@ -46,6 +47,63 @@ describe("Aggregate Foward", () => {
                     localField: "related",
                     foreignField: "_id",
                     as: "related",
+                },
+            },
+            { $unwind: "$related" },
+        ]);
+    });
+
+    it("should create pipeline deep 2", async () => {
+        const relatedSchema2 = new Schema(mongoose, {
+            name: { type: String, required: true },
+        });
+        const relatedSchema = new Schema(mongoose, {
+            name: { type: String, required: true },
+            related2: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "RelatedModel2",
+                required: true,
+            },
+        });
+        const testSchema = new Schema(mongoose, {
+            title: { type: String, required: true },
+            related: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "RelatedModel",
+                required: true,
+            },
+        });
+
+        Model(mongoose, "RelatedModel2", relatedSchema2);
+        Model(mongoose, "RelatedModel", relatedSchema);
+        const TestModel = Model(mongoose, "TestModel", testSchema);
+        await InitModels(client);
+
+        const generator = new GenerateFoward({ stop: {
+            collection: "",
+            bruteForce: false
+        }}, client);
+        const pipeline = await generator.makeAggregate(TestModel);
+        console.log(util.inspect(pipeline, { showHidden: false, depth: null, colors: true }));
+
+        expect(pipeline).toMatchObject([
+            {
+                $lookup: {
+                    from: "relatedmodels",
+                    localField: "related",
+                    foreignField: "_id",
+                    as: "related",
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: "relatedmodel2",
+                                localField: "related2",
+                                foreignField: "_id",
+                                as: "related2",
+                            },
+                        },
+                        { $unwind: "$related2" },
+                    ]
                 },
             },
             { $unwind: "$related" },
